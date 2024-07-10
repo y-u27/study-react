21969ebde1a5e08f5d3c116c20efcfb0d3222d57
Atd27 <y518a480@gmail.com>
2024-07-11 01:22:43 +0900

    ✨ クリックイベントの追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,8 +4,22 @@ import Styles from "./Styles";
 import { Footer } from "../components/Footer";
 import { Main } from "../components/Main";
 import { Header } from "../components/Header";
+import { useCallback } from "react";
+
+// const handleClick = (e) => {
+//   console.log(e.target.href);
+//   e.preventDefault();
+// };
 
 export default function Home() {
+  const foo = 1;
+
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo)
+  },[]);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,6 +27,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <a href="/about" onClick={handleClick}>
+        ボタン
+      </a>
       <Main page="Index" />
       <Footer />
       <Styles />
