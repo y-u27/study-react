db4ea16aa94e22336af7530c191e39ee69b8ecfb
Atd27 <y518a480@gmail.com>
2024-07-11 13:23:52 +0900

    ✨ counterの設置(useState)

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,7 @@ import Styles from "./Styles";
 import { Footer } from "../components/Footer";
 import { Main } from "../components/Main";
 import { Header } from "../components/Header";
-import { useCallback, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 // const handleClick = (e) => {
 //   console.log(e.target.href);
@@ -12,37 +12,34 @@ import { useCallback, useEffect } from "react";
 // };
 
 export default function Home() {
-  const foo = 1;
+  const [count, setCount] = useState(1);
 
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href);
-    e.preventDefault();
-    alert(foo)
-  },[]);
+  const handleClick = (e) => {
+    setCount((foo) => foo + 1);
+    setCount((foo) => foo + 1);
+  };
 
   useEffect(() => {
-    console.log("マウント時");
     // returnまでに書いている処理がマウント時
     document.body.style.backgroundColor = "lightblue";
     // return以降がアンマウント時の処理
     return () => {
-      console.log("アンマウント時");
       document.body.style.backgroundColor = "";
     };
     // []に変数などを入れることによって、useEffectやuseCallbackの処理をもう一度実行させることができる
   }, []);
 
   return (
-
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <a href="/about" onClick={handleClick}>
+      <h1>{count}</h1>
+      <button href="/about" onClick={handleClick}>
         ボタン
-      </a>
+      </button>
       <Main page="Index" />
       <Footer />
       <Styles />
