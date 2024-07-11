ce858ded11588e3bd1d0701c57d8c38058709aa5
Atd27 <y518a480@gmail.com>
2024-07-11 15:43:05 +0900

    🎨 Custom Hooksでまとめる

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -21,11 +21,11 @@ export default function About() {
       </Head>
       <Header />
       {/* useCounter */}
-      {isShow ? <h1>{count}</h1> : null}
       <button href="/about" onClick={handleClick}>
         ボタン
       </button>
       <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+      {isShow ? <h1>{count}</h1> : null}
       {/* useInputArray */}
       <input type="text" value={text} onChange={handleChange} />
       <button onClick={handleAdd}>追加</button>
